25d26031185ae58ac8c838ae2b9270e394a64387
muCkk <themuckk@gmx.net>
2012-09-04 20:46:29 +0200

    ItemTool

---
+++ src/main/java/net/minecraft/server/ItemTool.java
@@ -0,0 +1,73 @@
+package net.minecraft.server;
+
+import net.minecraftforge.common.ForgeHooks;
+
+public class ItemTool extends Item {
+
+    private Block[] c;
+    protected float a = 4.0F;
+    private int bY;
+    protected EnumToolMaterial b;
+
+    protected ItemTool(int i, int j, EnumToolMaterial enumtoolmaterial, Block[] ablock) {
+        super(i);
+        this.b = enumtoolmaterial;
+        this.c = ablock;
+        this.maxStackSize = 1;
+        this.setMaxDurability(enumtoolmaterial.a());
+        this.a = enumtoolmaterial.b();
+        this.bY = j + enumtoolmaterial.c();
+        this.a(CreativeModeTab.i);
+    }
+
+    public float getDestroySpeed(ItemStack itemstack, Block block) {
+        Block[] ablock = this.c;
+        int i = ablock.length;
+
+        for (int j = 0; j < i; ++j) {
+            Block block1 = ablock[j];
+
+            if (block1 == block) {
+                return this.a;
+            }
+        }
+
+        return 1.0F;
+    }
+
+    public boolean a(ItemStack itemstack, EntityLiving entityliving, EntityLiving entityliving1) {
+        itemstack.damage(2, entityliving1);
+        return true;
+    }
+
+    public boolean a(ItemStack itemstack, World world, int i, int j, int k, int l, EntityLiving entityliving) {
+        if ((double) Block.byId[i].m(world, j, k, l) != 0.0D) {
+            itemstack.damage(1, entityliving);
+        }
+
+        return true;
+    }
+
+    public int a(Entity entity) {
+        return this.bY;
+    }
+
+    public int b() {
+        return this.b.e();
+    }
+
+    public String e() {
+        return this.b.toString();
+    }
+    
+    /** FORGE: Overridden to allow custom tool effectiveness */
+    @Override
+    public float getStrVsBlock(ItemStack stack, Block block, int meta) 
+    {
+        if (ForgeHooks.isToolEffective(stack, block, meta))
+        {
+            return a;
+        }
+        return getDestroySpeed(stack, block);
+    }
+}
