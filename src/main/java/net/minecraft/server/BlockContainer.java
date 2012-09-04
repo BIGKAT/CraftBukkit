40aa41de223cda94e19967764909cd188c9085b0
muCkk <themuckk@gmx.net>
2012-09-04 03:31:48 +0200

    BlockContainer

---
+++ src/main/java/net/minecraft/server/BlockContainer.java
@@ -0,0 +1,40 @@
+package net.minecraft.server;
+
+public abstract class BlockContainer extends Block {
+
+    protected BlockContainer(int i, Material material) {
+        super(i, material);
+        this.isTileEntity = true;
+    }
+
+    protected BlockContainer(int i, int j, Material material) {
+        super(i, j, material);
+        this.isTileEntity = true;
+    }
+
+    public void onPlace(World world, int i, int j, int k) {
+        super.onPlace(world, i, j, k);
+        world.setTileEntity(i, j, k, this.createTileEntity(world, world.getData(i, j, k)));
+    }
+
+    public void remove(World world, int i, int j, int k, int l, int i1) {
+        super.remove(world, i, j, k, l, i1);
+        world.q(i, j, k);
+    }
+
+    public abstract TileEntity a(World world);
+    
+    public TileEntity createNewTileEntity(World world, int metadata)
+    {
+        return a(world);
+    }
+
+    public void b(World world, int i, int j, int k, int l, int i1) {
+        super.b(world, i, j, k, l, i1);
+        TileEntity tileentity = world.getTileEntity(i, j, k);
+
+        if (tileentity != null) {
+            tileentity.b(l, i1);
+        }
+    }
+}
